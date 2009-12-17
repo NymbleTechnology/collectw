864f82d8dd9ed0619aacc53737612981d1949027
Kayo Phoenix <kayo@1llumium.1on>
2009-12-17 11:39:50 +0500

    Info request now completed.

--- collectw.c
+++ collectw.c
@@ -1,18 +1,75 @@
-
+/** collectw.c --- 
+ *
+ * Copyright (C) 2009 Kayo Phoenix
+ *
+ * Author: Kayo Phoenix <kayo.k11.4@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include<dirent.h>
 #include<rrd.h>
 
 #include"collectw.h"
 
-static const char *collectw_rrd_basedir=NULL;
+static const char *collectw_rrd_basedir=COLLECTW_RRD_BASEDIR;
+static const char *collectw_user_config=COLLECTW_USER_CONFIG;
 
-int collectw_init(const char *rrd_basedir){
-  rrd_basedir && strlen(rrd_basedir) && (collectw_rrd_basedir=rrd_basedir) || (collectw_rrd_basedir=COLLECTW_RRD_BASEDIR);
+int collectw_init(const char *rrd_basedir, const char *user_config){
+  struct stat buf;
+  int ret=0;
+  
+  rrd_basedir && strlen(rrd_basedir) && (collectw_rrd_basedir=rrd_basedir);
+  user_config && strlen(user_config) && (collectw_user_config=user_config);
+  
+  if(stat(collectw_rrd_basedir, &buf)){
+    fprintf(stderr, "ERROR: RRD basedir set as \"%s\", but it's unavailable or not exists.\n", collectw_rrd_basedir);
+    ret++;
+  }else{
+    if(buf.st_mode&&S_IFDIR){
+      if(access(collectw_rrd_basedir, R_OK)){
+	fprintf(stderr, "ERROR: RRD basedir set as \"%s\", but it has no read access.\n", collectw_rrd_basedir);
+	ret++;
+      }
+    }else{
+      fprintf(stderr, "ERROR: RRD basedir set as \"%s\", but it's not a directory.\n", collectw_rrd_basedir);
+      ret++;
+    }
+  }
+  
+  if(stat(collectw_user_config, &buf)){
+    fprintf(stderr, "ERROR: User config set as \"%s\", but it's unavailable or not exists.\n", collectw_user_config);
+    ret++;
+  }else{
+    if(buf.st_mode&&S_IFREG){
+      if(access(collectw_user_config, R_OK)){
+	fprintf(stderr, "ERROR: User config set as \"%s\", but it has no read access.\n", collectw_user_config);
+	ret++;
+      }
+    }else{
+      fprintf(stderr, "ERROR: User config set as \"%s\", but it's not a file.\n", collectw_user_config);
+      ret++;
+    }
+  }
+  
+  return ret;
 }
 
 static int _collectw_info(Stream stream, const char *path){
@@ -43,6 +100,7 @@ static int _collectw_info(Stream stream, const char *path){
       rrd_info_t *info, *infos=rrd_info(2, arg);
       FPrintF(stream, "[");
       for(k=0, info=infos; info; info=info->next){
+#if 1
 	if(strstr(info->key, fp)==info->key && (end=strstr(info->key, bp))){
 	  if(k)FPrintF(stream, ","); k++;
 	  beg=info->key+strlen(fp);
@@ -52,6 +110,9 @@ static int _collectw_info(Stream stream, const char *path){
 	  name[len]='\0';
 	  FPrintF(stream, "'%s'", name);
 	}
+#else
+	FPrintF(stream, "'%s' ", info->key);
+#endif
       }
       FPrintF(stream, "]");
       if(infos)rrd_info_free(infos);  
@@ -64,12 +125,15 @@ static int _collectw_info(Stream stream, const char *path){
   return 0;
 }
 
-int collectw_info(Stream stream){
+int collectw_none(Stream stream, const char **param){
+  FPrintF(stream, "{}");
+  return 0;
+}
+
+int collectw_info(Stream stream, const char **param){
   return _collectw_info(stream, collectw_rrd_basedir);
 }
 
-int collectw_data(Stream stream, const char *path){
+int collectw_data(Stream stream, const char **param){
   
-  //char * arg[] = { 0, c_file, 0 };
-  //return rrd_info(2, arg);
 }
--- collectw.h
+++ collectw.h
@@ -1,14 +1,38 @@
-#ifndef __COLLECTW_HEADER__
-#define __COLLECTW_HEADER__
+/** collectw.h --- 
+ *
+ * Copyright (C) 2009 Kayo Phoenix
+ *
+ * Author: Kayo Phoenix <kayo.k11.4@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __COLLECTW_H__
+#define __COLLECTW_H__
 
 #ifndef COLLECTW_INTERFACE
 #error You must use one of interfaces.
 #else
 #include COLLECTW_INTERFACE
 
-int collectw_init(const char *rrd_basedir);
-int collectw_info(Stream stream);
-int collectw_data(Stream stream, const char *path);
+int collectw_init(const char *rrd_basedir,
+		  const char *user_config);
+
+int collectw_none(Stream stream, const char **param);
+int collectw_info(Stream stream, const char **param);
+int collectw_data(Stream stream, const char **param);
 
 #endif
-#endif
+
+#endif//__COLLECTW_H__
