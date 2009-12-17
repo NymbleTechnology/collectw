226e2d439da224951b7be4f6ba4c01c2dd2e2d98
Kayo Phoenix <kayo@1llumium.1on>
2009-12-17 13:54:20 +0500

    Query parsing refactored

--- sapi_fcgx.c
+++ sapi_fcgx.c
@@ -30,29 +30,6 @@
 
 #define TRY(code) if(code){fprintf(stderr,"Fatal error: " #code ".. Exiting..\n");exit(3);}
 
-/*
-const char **arguments_token(const char *query_string){
-  int len=strlen(query_string), i, n, t;
-  char *args_line=malloc(len+1), **args;
-  strcpy(args_line, query_string);
-  args_line[len]='\0';
-  for(n=0,i=0;i<len;i++)if(args_line[i]=='&')n++;
-  args=malloc(n);
-  args[0]=args_line;
-  for(t=1,i=0;i<len;i++)if(args_line[i]=='&'){
-      args_line[i]='\0';
-      args[t]=args_line+i+1;
-      t++;
-    }
-  return args;
-}
-
-void arguments_free(const char **args){
-  free(args[0]);
-  free(args);
-}
-*/
-
 help(){
   printf(COLLECTW_FCGX_HELP "\n");
 }
@@ -81,37 +58,39 @@ main(int argc, char **argv){
     }
   }
   
+  fprintf(stderr, "Initializing..\n");
   /* Init collectw */
   if(collectw_init(rrddir, config)) exit(2);
   
   /* Init queries */
   struct{
-    const char *re;
-    unsigned char n;
-    regex_t *pg;
     int(*cb)(Stream stream, const char **param);
+    regex_t *pg;
+    const char *re;
+    const char *rs;
   }reque[]={
     // url schema: collectw?info
-    {"^info$", 0,
-     NULL, collectw_info},
+    {collectw_info, NULL, "^info$", "~"},
     // url schema: collectw?data:PATH(DS)[START,END]
-    {"^data:([[:alnum:]\\_\\.\\-]+\\.rrd)\\(([[:alpha:]]+)\\)\\[[[:digit:]]{4}\\-[[:digit:]]{2}\\-[[:digit:]]{2}[[:space:]][[:digit:]]{2}\\:[[:digit:]]{2}\\,\\]$", 5,
-     NULL, collectw_data},
+    {collectw_data, NULL, "^data:([[:alnum:]\\_\\.\\-]+\\.rrd)\\(([[:alpha:]]+)\\)\\[[[:digit:]]{4}\\-[[:digit:]]{2}\\-[[:digit:]]{2}[[:space:]][[:digit:]]{2}\\:[[:digit:]]{2}\\,\\]$", "~\1\2\3\4\5"},
     // any others
-    {"^.*$", 0,
-     NULL, collectw_none},
-    {NULL, 0,
-     NULL, NULL}
+    {collectw_none, NULL, "^.*$", "~"},
+    {NULL, NULL, NULL, NULL}
   };
   unsigned char max_n=0;
   
   {
 #define ERRBUF_SIZE 256
+    const char *rp;
+    unsigned char n;
     char errs=0;
     char errbuf[ERRBUF_SIZE];
     for(i=0;reque[i].re;i++){
+      /* calculate number of substitutions */
+      for(n=0,rp=reque[i].re;rp=strchr(rp, '(');(rp==reque[i].re || *(rp-1)!='\\') && n++, rp++);
+      max_n=n>max_n?n:max_n;
+      /* initialize regexp */
       reque[i].pg=malloc(sizeof(regex_t));
-      max_n=max_n<reque[i].n?reque[i].n:max_n;
       c=regcomp(reque[i].pg, reque[i].re, REG_EXTENDED);
       if(c){
 	regerror(c, reque[i].pg, errbuf, ERRBUF_SIZE);
@@ -119,6 +98,7 @@ main(int argc, char **argv){
 	errs++;
       }
     }
+    max_n++;
     if(errs>0){
       exit(2);
     }
@@ -133,15 +113,17 @@ main(int argc, char **argv){
   TRY( FCGX_InitRequest(&request, socket, 0) );
   
   /* Init main cicle */
+  fprintf(stderr, "Running main cicle..\n");
   {
     regmatch_t match[max_n];
     unsigned char n,l;
     char *param[max_n];
     
+    for(n=0;n<max_n;n++)param[n]=NULL;
+    
     for (;;) {
       TRY( FCGX_Accept_r(&request) );
       query=FCGX_GetParam("QUERY_STRING", request.envp);
-      //args=arguments_token(query);
       //FPrintF(request.out, "Content-Type: application/json; charset=utf-8\r\n");
       FPrintF(request.out, "Content-Type: text/plain; charset=utf-8\r\n");
       //FPrintF(request.out, "X-JSON: ['ok']\r\n");
@@ -160,14 +142,12 @@ main(int argc, char **argv){
 	    if(REG_NOMATCH==regexec(reque[i].pg, query, max_n, match, 0))continue;
 	    
 	    /* Fill params */
-	    for(n=0;n<reque[i].n;n++){
-	      if(match[n].rm_so>0){
+	    for(n=0;n<reque[i].rs[n];n++){
+	      if(match[n].rm_so>0 && reque[i].rs[n]!='~'){
 		l=match[n].rm_eo-match[n].rm_so;
-		param[n]=malloc(l+1);
-		strncpy(param[n], query+match[n].rm_so, l);
-		param[n][l]='\0';
-	      }else{
-		param[n]=NULL;
+		param[reque[i].rs[n]]=malloc(l+1);
+		strncpy(param[reque[i].rs[n]], query+match[n].rm_so, l);
+		param[reque[i].rs[n]][l]='\0';
 	      }
 	    }
 	    
@@ -175,7 +155,7 @@ main(int argc, char **argv){
 	    reque[i].cb(request.out, (const char **)param);
 	    
 	    /* Clear params */
-	    for(n=0;n<reque[i].n;n++){
+	    for(n=0;n<reque[i].rs[n];n++){
 	      if(param[n])free(param[n]);
 	      param[n]=NULL;
 	    }
@@ -186,13 +166,13 @@ main(int argc, char **argv){
 	}
 #endif
       }
+      
+      FPrintF(request.out, "\r\n");
+      FCGX_Finish_r(&request);
     }
-    
-    //arguments_free(args);
-    FPrintF(request.out, "\r\n");
-    FCGX_Finish_r(&request);
+    fprintf(stderr, "Exiting..\n");
   }
-
+  
   /* Freeing regexes */
   for(i=0;reque[i].re;i++){
     regfree(reque[i].pg);
